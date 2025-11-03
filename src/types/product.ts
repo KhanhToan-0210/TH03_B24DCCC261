424799fa754ec102286df3c159abf2ef28af488f
KhanhToan-0210 <Khanhtoan021006@gmail.com>
2025-11-03 14:47:53 +0700

    Update product.ts

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+// src/types/product.ts
+export type Category = 'Điện tử' | 'Quần áo' | 'Đồ ăn' | 'Sách' | 'Khác';
+
+export interface Product {
+  id: number;
+  ten: string;
+  danhMuc: Category;
+  gia: number;
+  soLuong: number;
+  moTa: string;
+}
